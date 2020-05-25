9a868275608d7fcfb8d2133510c5f400b28d6e6e
mattiasala <mattiasala09@gmail.com>
2020-05-25 23:14:46 +0200

    Added Beans: Optional, Order, Product

---
+++ TIW-EstimatesManagementPureHTML/src/it/polimi/tiw/estimates/beans/Order.java
@@ -0,0 +1,36 @@
+package it.polimi.tiw.estimates.beans;
+
+public class Order {
+	private int id;
+	private int client; 
+	private int employee;
+	private float price;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	public int getClient() {
+		return client;
+	}
+	public void setClient(int client) {
+		this.client = client;
+	}
+	
+	public int getEmployee() {
+		return employee;
+	}
+	public void setEmployee(int employee) {
+		this.employee = employee;
+	}
+	
+	public float getPrice() {
+		return price;
+	}
+	public void setPrice(float price) {
+		this.price = price;
+	}
+}
