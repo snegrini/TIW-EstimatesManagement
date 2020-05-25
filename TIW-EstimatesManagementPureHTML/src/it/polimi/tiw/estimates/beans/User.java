9637f5616d4bc3437c26eae811b23b4dbb19e1a0
Samuele Negrini <samuele.negrini@mail.polimi.it>
2020-05-25 21:43:13 +0200

    Create package it.polimi.tiw.estimates.beans

---
+++ TIW-EstimatesManagementPureHTML/src/it/polimi/tiw/estimates/beans/User.java
@@ -0,0 +1,33 @@
+package it.polimi.tiw.estimates.beans;
+
+public class User {
+	private int id;
+	private String username;
+	private String name;
+	private String surname;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
