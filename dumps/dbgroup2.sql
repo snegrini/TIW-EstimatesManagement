20966705af816215ffc484a62bc7c345e6c3c368
Samuele Negrini <samuele.negrini@mail.polimi.it>
2020-06-11 15:40:54 +0200

    Update DB dump

--- dumps/dbgroup2.sql
+++ dumps/dbgroup2.sql
@@ -26,11 +26,58 @@ CREATE TABLE IF NOT EXISTS `chosenoptional` (
   CONSTRAINT `FK_chosenoptional_optional` FOREIGN KEY (`optid`) REFERENCES `optional` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
--- Dumping data for table dbgroup2.chosenoptional: ~0 rows (approximately)
+-- Dumping data for table dbgroup2.chosenoptional: ~49 rows (approximately)
 /*!40000 ALTER TABLE `chosenoptional` DISABLE KEYS */;
 INSERT INTO `chosenoptional` (`estid`, `optid`) VALUES
-	(1, 3),
-	(2, 9);
+	(14, 8),
+	(15, 5),
+	(16, 3),
+	(16, 4),
+	(17, 3),
+	(17, 4),
+	(19, 8),
+	(20, 2),
+	(21, 4),
+	(21, 7),
+	(22, 3),
+	(23, 3),
+	(24, 1),
+	(24, 2),
+	(25, 3),
+	(25, 9),
+	(26, 7),
+	(27, 8),
+	(28, 2),
+	(29, 4),
+	(29, 7),
+	(30, 3),
+	(31, 4),
+	(31, 7),
+	(32, 1),
+	(32, 7),
+	(32, 8),
+	(33, 1),
+	(34, 1),
+	(34, 2),
+	(35, 4),
+	(35, 7),
+	(36, 1),
+	(36, 2),
+	(37, 3),
+	(38, 4),
+	(39, 3),
+	(40, 3),
+	(41, 7),
+	(42, 3),
+	(42, 5),
+	(42, 9),
+	(42, 10),
+	(43, 3),
+	(43, 7),
+	(44, 3),
+	(44, 7),
+	(45, 8),
+	(46, 5);
 /*!40000 ALTER TABLE `chosenoptional` ENABLE KEYS */;
 
 -- Dumping structure for table dbgroup2.estimate
@@ -46,13 +93,43 @@ CREATE TABLE IF NOT EXISTS `estimate` (
   KEY `FK_order_user` (`empid`),
   CONSTRAINT `FK_estimate_product` FOREIGN KEY (`prdid`) REFERENCES `product` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
   CONSTRAINT `FK_order_user` FOREIGN KEY (`empid`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=47 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
--- Dumping data for table dbgroup2.estimate: ~0 rows (approximately)
+-- Dumping data for table dbgroup2.estimate: ~32 rows (approximately)
 /*!40000 ALTER TABLE `estimate` DISABLE KEYS */;
 INSERT INTO `estimate` (`id`, `usrid`, `prdid`, `empid`, `price`) VALUES
-	(1, 2, 4, 1, 100),
-	(2, 3, 2, NULL, NULL);
+	(14, 2, 6, 1, 456789),
+	(15, 2, 2, 1, 71),
+	(16, 4, 1, 1, 85),
+	(17, 4, 1, 1, 647384),
+	(19, 3, 6, 1, 55),
+	(20, 4, 3, 1, 999),
+	(21, 4, 5, 1, 222),
+	(22, 3, 4, 1, 50),
+	(23, 3, 1, 1, 24.4444),
+	(24, 3, 3, 1, 20.2),
+	(25, 3, 2, 1, 896),
+	(26, 3, 4, 1, 66),
+	(27, 3, 6, 1, 88),
+	(28, 3, 3, 1, 55),
+	(29, 3, 5, 1, 88),
+	(30, 2, 1, 1, 10),
+	(31, 2, 5, 1, 22.455),
+	(32, 2, 6, 1, 44),
+	(33, 2, 3, 1, 5),
+	(34, 2, 3, 1, 0),
+	(35, 2, 5, 1, 24.4),
+	(36, 2, 3, 1, 0.25),
+	(37, 2, 1, 1, 44),
+	(38, 2, 1, 1, 22.44),
+	(39, 4, 2, NULL, NULL),
+	(40, 4, 4, 1, 55),
+	(41, 4, 5, NULL, NULL),
+	(42, 4, 2, 1, 44.56),
+	(43, 2, 4, 1, 0),
+	(44, 2, 4, NULL, NULL),
+	(45, 2, 6, 1, 33222),
+	(46, 4, 2, NULL, NULL);
 /*!40000 ALTER TABLE `estimate` ENABLE KEYS */;
 
 -- Dumping structure for table dbgroup2.optional
@@ -72,7 +149,7 @@ INSERT INTO `optional` (`id`, `name`, `type`) VALUES
 	(4, 'Tendine parasole', 'NORMAL'),
 	(5, 'Sedili in ecopelle', 'SALE'),
 	(7, 'Wi-Fi', 'NORMAL'),
-	(8, 'Motore a fissione nucleare', 'NORMAL'),
+	(8, 'Propulsione nucleare ad impulso', 'NORMAL'),
 	(9, 'Vagone bar', 'SALE'),
 	(10, 'Vetri fotocromatici', 'NORMAL');
 /*!40000 ALTER TABLE `optional` ENABLE KEYS */;
@@ -87,17 +164,24 @@ CREATE TABLE IF NOT EXISTS `optionaltoproduct` (
   CONSTRAINT `FK_optionaltoproduct_product` FOREIGN KEY (`prdid`) REFERENCES `product` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
--- Dumping data for table dbgroup2.optionaltoproduct: ~8 rows (approximately)
+-- Dumping data for table dbgroup2.optionaltoproduct: ~15 rows (approximately)
 /*!40000 ALTER TABLE `optionaltoproduct` DISABLE KEYS */;
 INSERT INTO `optionaltoproduct` (`prdid`, `optid`) VALUES
 	(1, 3),
+	(1, 4),
 	(2, 3),
 	(2, 5),
 	(2, 9),
+	(2, 10),
 	(3, 1),
+	(3, 2),
 	(4, 3),
 	(4, 7),
-	(5, 7);
+	(5, 4),
+	(5, 7),
+	(6, 1),
+	(6, 7),
+	(6, 8);
 /*!40000 ALTER TABLE `optionaltoproduct` ENABLE KEYS */;
 
 -- Dumping structure for table dbgroup2.product
@@ -106,16 +190,17 @@ CREATE TABLE IF NOT EXISTS `product` (
   `name` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
   `image` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
--- Dumping data for table dbgroup2.product: ~5 rows (approximately)
+-- Dumping data for table dbgroup2.product: ~6 rows (approximately)
 /*!40000 ALTER TABLE `product` DISABLE KEYS */;
 INSERT INTO `product` (`id`, `name`, `image`) VALUES
 	(1, 'ETR 425 CORADIA MERIDIAN', 'ETR_425_CORADIA_MERIDIAN.jpg'),
 	(2, 'ETR 245 CORADIA MERIDIAN', 'ETR_245_CORADIA_MERIDIAN.jpg'),
 	(3, 'Elettrotreno TSR', 'ELETTROTRENO_TSR.jpg'),
 	(4, 'Elettrotreno TAF', 'ELETTROTRENO_TAF.jpg'),
-	(5, 'Elettromotrice ALe 582', 'ELETTROMOTRICE_ALe_582.jpg');
+	(5, 'Elettromotrice ALe 582', 'ELETTROMOTRICE_ALe_582.jpg'),
+	(6, 'ETR 621 CARAVAGGIO', 'ETR_621_CARAVAGGIO.jpeg');
 /*!40000 ALTER TABLE `product` ENABLE KEYS */;
 
 -- Dumping structure for table dbgroup2.user
@@ -130,14 +215,17 @@ CREATE TABLE IF NOT EXISTS `user` (
   PRIMARY KEY (`id`) USING BTREE,
   UNIQUE KEY `username` (`username`),
   UNIQUE KEY `email` (`email`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
--- Dumping data for table dbgroup2.user: ~1 rows (approximately)
+-- Dumping data for table dbgroup2.user: ~6 rows (approximately)
 /*!40000 ALTER TABLE `user` DISABLE KEYS */;
 INSERT INTO `user` (`id`, `username`, `password`, `email`, `name`, `surname`, `role`) VALUES
 	(1, 'employee1', 'employee1pass', 'mail2@example.org', 'Aldo', 'Plenzich', 'employee'),
 	(2, 'customer1', 'bellanapoli123', 'LuigiDiMAyo@esteri.gov.it', 'Luigi', 'Di Maio', 'customer'),
-	(3, 'mattiasala', '12345', 'mattiasala@polimi.it', 'Mattia', 'Sala', 'customer');
+	(3, 'mattiasala', '12345', 'mattiasala@polimi.it', 'Mattia', 'Sala', 'customer'),
+	(4, 'snegrini', 'aassddff', 'negrinisamuele@outlook.com', 'Samuele', 'Negrini', 'customer'),
+	(5, '@!u_', '@!u_', 'ws@dd.cca', '@!u_', '@!u_', 'customer'),
+	(6, 'testuser', 'test123', 'testuser@testmail.com', 'testname', 'testsurname', 'customer');
 /*!40000 ALTER TABLE `user` ENABLE KEYS */;
 
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
